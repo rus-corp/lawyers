635a2eaa6cc1e75e07017b54b087e6149ca86217
Ruslan Prusakov <ruslan.prusakov@gmail.com>
2025-06-15 19:19:09 +0100

    fix search

--- frontend/src/app/payment_page/page.tsx
+++ frontend/src/app/payment_page/page.tsx
@@ -1,61 +1,10 @@
-"use client";
-import YookassaComponent from "@/components/yookassa/YokassaComponent";
-import React, { useEffect, useState, useRef } from "react";
-import { useRouter } from 'next/navigation';
-import { useSearchParams } from 'next/navigation';
-import style from './payment_page.module.css'
+import { Suspense } from 'react';
+import PaymentPageClient from './PaymentPageClient';
 
-import { createNewOrder } from "@/api";
-import { CreateOrderData } from "./types";
-import { CreateItemInput } from "@/ui/inputs/SearchInput";
-
-
-const PaymentPage = () => {
-  const hasSentRef = useRef(false);
-  const searchParams = useSearchParams();
-  const amount = searchParams.get('amount');
-  const documentId = searchParams.get('documentId');
-  const userEmail = searchParams.get('userEmail')
-  const [confirmationToken, setConfirmationToken] = useState<string | null>(null);
-  const returnUrl = '/';
-
-
-  const handleCreateOrder = async (createOrderData: CreateOrderData) => {
-    const response = await createNewOrder(createOrderData);
-    if (response?.status === 201) {
-      setConfirmationToken(response.data.confirmation_token);
-    }
-  }
-
-
-
-  useEffect(() => {
-    if (amount && documentId && userEmail && !hasSentRef.current) {
-      const data = {
-        price: Number(amount),
-        description: String(documentId),
-        user_email: String(userEmail)
-      }
-      handleCreateOrder(data)
-      hasSentRef.current = true;
-    }
-  }, [])
-
+export default function PaymentPage() {
   return (
-    <section className={style.paymentPage}>
-      <div className='container'>
-        {confirmationToken ? (
-          <>
-            <h2>Оплата</h2>
-            <YookassaComponent paymentToken={confirmationToken} returnUrl={returnUrl} />
-          </>
-
-        ) : (
-          <p>Загрузка формы оплаты...</p>
-        )}
-      </div>
-    </section>
+    <Suspense fallback={<p>Загрузка...</p>}>
+      <PaymentPageClient />
+    </Suspense>
   );
-};
-
-export default PaymentPage;
+}
