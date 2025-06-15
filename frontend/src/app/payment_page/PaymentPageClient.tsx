f1e3ffdf91dcbcc3d6d3b1b57c27466ea0d1b034
Ruslan Prusakov <ruslan.prusakov@gmail.com>
2025-06-15 19:28:52 +0100

    kass fix

--- frontend/src/app/payment_page/PaymentPageClient.tsx
+++ frontend/src/app/payment_page/PaymentPageClient.tsx
@@ -15,7 +15,7 @@ const PaymentPage = () => {
   const documentId = searchParams.get('documentId');
   const userEmail = searchParams.get('userEmail')
   const [confirmationToken, setConfirmationToken] = useState<string | null>(null);
-  const returnUrl = '/';
+  const returnUrl = 'https://сам-себе-юрист.рф/payment/success';
 
 
   const handleCreateOrder = async (createOrderData: CreateOrderData) => {
