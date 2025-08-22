039d1938a3ea6e66676a608bb48201e4287a1e4e
Ruslan Prusakov <ruslan.prusakov@gmail.com>
2025-08-22 10:41:11 +0100

    set

--- backend/lawyers_back/settings.py
+++ backend/lawyers_back/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
 }
 
 
